b3e58773b2b458de74ed9d29847e4cc48df1dec1
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2023-07-23 18:16:48 +0530

    updated

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+const GroupLink = () => {
+window.open("https://chat.whatsapp.com/Ifc2cxkRaBkJf9LUCXYzgD")
+}
+
+// loader 
+window.addEventListener('load', function() {
+    var loadingAnimation = document.getElementById('loading-animation');
+    loadingAnimation.style.display = 'none';
+  });
+  
